df8430910689d054659ad1ba0734fe77ba91c518
Yasinahamad <163731345+yasinahamed2005@users.noreply.github.com>
2025-08-06 10:14:13 +0530

    Update Aboutus.jsx

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -6,6 +6,7 @@ const AboutUs = () => {
   return (
     <div className="about-container">
       <h2>About Our Electronic Store</h2>
+      <h1>Velacomu bro</h1>
       <p>
         Welcome to our E-Commerce platform, your one-stop shop for the latest and greatest in electronics!
         We specialize in high-quality products, including computers, mobiles, laptops, and accessories.
